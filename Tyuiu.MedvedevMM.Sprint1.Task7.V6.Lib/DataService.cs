509f1f83425e437980edd9c9d2dd81d36cc3f017
MedvedevMM <Tyuiu.MedvedevMM@outlook.com>
2024-10-05 08:12:29 +0500

    Task7

--- Tyuiu.MedvedevMM.Sprint1.Task7.V6.Lib/DataService.cs
+++ Tyuiu.MedvedevMM.Sprint1.Task7.V6.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MedvedevMM.Sprint1.Task7.V6.Lib
         public double Calculate(double x, double y)
         {
             var res = Math.Pow(1 + (1 / Math.Pow(x, 2)), x) - 12 * Math.Pow(x, 2) * y;
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }
